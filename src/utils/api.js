97b6fb6e0c3a37a8dbbe28b8f48d2d0884450d3b
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-27 19:12:45 -0400

    Declare hacker news api fetchStory function within api.js file

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,16 @@
 // Hacker News - base url.
 const baseUrl = 'https://hacker-news.firebaseio.com/v0/'
 
+// Fetch hacker news story by id.
+const fetchStory = id => {
+	return fetch(`${baseUrl}/item/${id}.json`)
+		.then(res => res.json())
+		.then(story => story)
+		.catch(e => {
+			throw new Error(e);
+		})
+};
+
 // Fetch top stories from hacker news API.
 const fetchTopStoryIds = () => {
 	return fetch(`${baseUrl}topstories.json`)
