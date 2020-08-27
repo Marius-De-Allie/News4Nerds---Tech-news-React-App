fa75567c492e6bb83dbdf884e048310a139a6d47
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-27 19:34:12 -0400

    Declare hacker news fetchStories async function

--- src/utils/api.js
+++ src/utils/api.js
@@ -22,6 +22,24 @@ const fetchTopStoryIds = () => {
 		})
 };
 
+// Fetch array of story objects.
+const fetchStories = async () => {
+	try {
+		// 1. Get list of top 50 story ids from HN.
+		const ids = await fetchTopStoryIds();
+		// Get only the first 50 story ids from ids array.
+		const storyIds = ids.slice(null, 50);
+		// 2. Loop over ids and fetch each corresponding item and update array.
+		storyIds.forEach(async id => {
+			const item = await fetchItem(id);
+			return item
+		});
+	} catch {
+		throw new Error('Unable to fetch stories')
+	}
+};
+
 export {
+	fetchStories as default,
 	fetchTopStoryIds
 }
