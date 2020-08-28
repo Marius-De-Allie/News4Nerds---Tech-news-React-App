d7d22662929080551c4d0e47713660efa793042e
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-28 14:08:24 -0400

    Create Nav functional component

---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Nav = () => (
+    <div className='nav-bar'>
+        <ul className='nav-items'>
+            <li>Top</li>
+            <li>New</li>
+        </ul>
+        <button 
+            className='theme-btn'
+            onClick={() => {}}
+        >
+            {`🔦`}
+        </button>
+    </div>
+);
+
+export default Nav;
