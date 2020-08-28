79b227aaf5832988437d251f03063601a9291d99
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-28 12:13:46 -0400

    Create 'CommentsList' functional  component

---
+++ src/components/CommentsList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CommentsList = ({ comments }) => (
+    <div className='comments-list-container'>
+        <ul>
+            CommentsList
+        </ul>
+    </div>
+);
+
+export default CommentsList;
