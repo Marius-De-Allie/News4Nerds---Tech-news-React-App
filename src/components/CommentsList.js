a1e92b717436d48ff2319f71d76d2c6b60e8eb79
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-28 12:16:02 -0400

    Declare proptyyes from CommentsList component

--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
 const CommentsList = ({ comments }) => (
     <div className='comments-list-container'>
@@ -7,5 +8,10 @@ const CommentsList = ({ comments }) => (
         </ul>
     </div>
 );
+
+// CommentsList proptypes.
+CommentsList.propTypes = {
+    comments: propTypes.array.isRequired
+};
 
 export default CommentsList;
