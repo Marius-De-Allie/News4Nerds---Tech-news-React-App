6f1df18569ff2c55e9543aa75d9b47be30bfe5ef
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-28 11:31:33 -0400

    Create StoryList function component

---
+++ src/components/StoryList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StoryList = ( { stories }) => (
+    <div className='story-list-container'>
+        <ul>
+            StoryList
+        </ul>
+    </div>
+);
+
+export default StoryList;
