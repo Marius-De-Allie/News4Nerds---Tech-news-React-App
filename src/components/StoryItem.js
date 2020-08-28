f2ca159462446b9df9d3f6fa287fa54f0001d8f0
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-28 15:34:15 -0400

    Update StoryItem component to render contents of story passed in as props

--- src/components/StoryItem.js
+++ src/components/StoryItem.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
-const StoryItem = props => (
+const StoryItem = ({ by, descendants, id, kids, score, time, title, type, url }) => (
     <li>
         {/* TODO add avatar/icon */}
-        <h2>Story Header</h2>
-        <p>{`Story details: by user 👨‍💻`}</p>
+        <a 
+            className='story-link'
+            href={url} target='_blank' 
+            rel='noopener noreferrer'
+        >
+            <h2>{title}</h2>
+        </a>
+        <p>
+            {'👨‍💻Posted by'}
+            <a href={url}>{` ${by} `}</a>
+            on {`${time},`}
+            <a href={url}>{` 0 `}</a>comments
+        </p>
     </li>
 );
 
 export default StoryItem;
+
+
