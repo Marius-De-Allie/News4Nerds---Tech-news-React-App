29ceab9c7c3d7670769a4021d458ef63f0dea446
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-28 11:48:41 -0400

    Create StoryItem class component

---
+++ src/components/StoryItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StoryItem = props => (
+    <li>
+        {/* TODO add avatar/icon */}
+        <h2>Story Header</h2>
+        <p>{`Story details: by user 👨‍💻`}</p>
+    </li>
+);
+
+export default StoryItem;
