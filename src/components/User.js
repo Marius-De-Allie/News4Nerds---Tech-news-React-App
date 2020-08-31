790bcf7772c0583a408453b99388b608aa342bcd
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-31 11:38:48 -0400

    Update User component's componentDidMount lifecycle method to an async function and cache userDetails peice of component state within lifecycle method

--- src/components/User.js
+++ src/components/User.js
@@ -9,28 +9,39 @@ class User extends Component {
 
 		this.state = {
 			userDetails: null,
-			userStories: null
+			userStories: {}
 		}
 	}
 
-	componentDidMount() {
+	async componentDidMount() {
 		const { location } = this.props;
 		// 1. Retrieve query string from url and parse into an object.
 		const { id } = queryString.parse(location.search);
 		// 2 .Fetch user details from HN API.
-		fetchUser(id)
-			.then(userDetails => {
-				this.setState(prevState => ({
-					userDetails
-				}));
-				// 3. Fetch user's stories from HN.
-				fetchUserStories(userDetails.submitted)
-					.then(userStories => this.setState(prevState => ({
-						userStories
-					})))
-					.catch(e => console.warn(e));
-			})
-			.catch(e => console.warn(e));
+		if(!this.state.userDetails[id]) {
+			const userDetails = await fetchUser(id);
+			this.setState(prevState => ({
+				userDetails: {
+					...prevState.userDetails,
+					[userDetails.id]: {...userDetails}
+				}
+			}))
+		}
+
+
+		// fetchUser(id)
+		// 	.then(userDetails => {
+		// 		this.setState(prevState => ({
+		// 			userDetails
+		// 		}));
+		// 		// 3. Fetch user's stories from HN.
+		// 		fetchUserStories(userDetails.submitted)
+		// 			.then(userStories => this.setState(prevState => ({
+		// 				userStories
+		// 			})))
+		// 			.catch(e => console.warn(e));
+		// 	})
+		// 	.catch(e => console.warn(e));
 	}
 
 	render() {
