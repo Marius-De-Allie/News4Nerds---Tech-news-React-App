448dfb2f12a74781baddcb6e544b013abe949c95
Marius-De-Allie <spectre4300s@yahoo.com>
2020-08-30 23:27:17 -0400

    Wrap App component's rendered JSX in a ThemeContext.Provider component, set context Provider's value prop to App component's state object

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import New from './components/New';
 import User from './components/User';
 import Comments from './components/Comments';
+import ThemeContext from './contexts/theme';
 
 class App extends React.Component {
   constructor(props) {
@@ -22,17 +23,19 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <Nav />
-        <Switch>
-          <div className='main-container' style={{maxWidth: '1000px', margin: '0 auto'}}>
-            <Route exact path='/' component={Home} />
-            <Route path='/new' component={New} />
-            <Route path='/user' component={User} />
-            <Route path='/post' component={Comments} />
-          </div>
-        </Switch>
-      </div>
+      <ThemeContext.Provider>
+        <div>
+          <Nav />
+          <Switch>
+            <div className='main-container' style={{maxWidth: '1000px', margin: '0 auto'}}>
+              <Route exact path='/' component={Home} />
+              <Route path='/new' component={New} />
+              <Route path='/user' component={User} />
+              <Route path='/post' component={Comments} />
+            </div>
+          </Switch>
+        </div>
+      </ThemeContext.Provider>
     );
   }
 }
