3bdd2bc983d33d80d18695d127bf674791fca8fa
Marius-De-Allie <spectre4300s@yahoo.com>
2020-09-01 12:25:41 -0400

    Update Comment component's componentDidMount lifecycle method to fetch story's comments and update locacl component state accordingly.

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import queryString from 'query-string';
-import { fetchItem, fetchComments } from '../utils/api';
+import { fetchItem} from '../utils/api';
 import CommentsList from './CommentsList';
 
 class Comments extends Component {
@@ -14,55 +14,68 @@ class Comments extends Component {
 		}
 	}
 
-	async componentDidMount() {
+	componentDidMount() {
 		// retrieve post id value from url query string.
 		const { id } = queryString.parse(this.props.location.search);
 
 		const { postDetails, comments } = this.state;
 
 		if(!postDetails[id]) {
-			const post = await fetchItem(id);
-				this.setState(prevState => ({
-					postDetails: {
-						...prevState.postDetails,
-						[id]: {...post}
-					}
-				}));
+			fetchItem(id)
+				.then(item => {
+					this.setState(prevState => ({
+						postDetails: {
+							...prevState.postDetails,
+							[item.id]: {...item}
+						}
+					}))
 
-
-				const commentIds = postDetails[id].kids.slice(null, 50);
-				for(let i =0; i < commentIds.length; i++) {
-					if(!comments[commentIds[i]]) {
-						// TODO call fetchComment fn.
-						// TODO set comments state equal to new comment, plus existing comments.
+					const commentIds = item.kids.slice(null, 50);
+					for(let i =0; i < commentIds.length; i++) {
+						if(!comments[commentIds[i]]) {
+							fetchItem(commentIds[i])
+								.then(comment => {
+									this.setState(prevState => ({
+										comments: {
+											...prevState.comments,
+											[comment.id]: {...comment}
+										}
+									}));
+								})
+								.catch(e => console.warn(e));
+						}
 					}
-				}
-		}
+				})
+				.catch(e => console.warn(e));
+			}
 	}
 
 
 	render() {
-		const { comments, postDetails } = this.state;
+		const urlQuerystring = queryString.parse(this.props.location.search);
+		const { postDetails } = this.state;
+		const ids = Object.keys(this.state.comments);
+		const comments = ids.map(id => this.state.comments[id]);
 			return (
 				<React.Fragment>
-						{postDetails != null ? (
+						{postDetails[urlQuerystring.id] ? (
 							<div className='ui fluid raised card'>
 								<div className='content'>
-									<h2>{postDetails.title}</h2>
+									<h2>{postDetails[urlQuerystring.id].title}</h2>
 								</div>
 								<div className='content'>
-									{JSON.stringify(postDetails.kids)}
+									{JSON.stringify(postDetails[urlQuerystring.id].kids)}
 									<p className='description'>
 										by
 										<Link
 											to={{
 												pathname: '/user',
-												search: `?id=${postDetails.by}`
+												search: `?id=${postDetails[urlQuerystring.id].by}`
 											}}
 										>
-											{` ${postDetails.by}`}
+											{` ${postDetails[urlQuerystring.id].by} `}
 										</Link>
-										on {` ${new Date(postDetails.time * 1000).toLocaleString()} `}{`${postDetails.descendants} comments`} 
+										on {` ${new Date(postDetails[urlQuerystring.id].time * 1000).toLocaleString()} `}{`${postDetails[urlQuerystring.id].descendants} comments`} 
 									</p>
 								</div>
 							</div>
