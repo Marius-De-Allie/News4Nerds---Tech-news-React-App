803a6d52cb2e1b182601df4d766d7c86469854fe
Marius-De-Allie <spectre4300s@yahoo.com>
2020-09-01 15:48:52 -0400

    Wrap CommentItem component in ThemeContext.Consumer component to apply theme styling.

--- src/components/CommentItem.js
+++ src/components/CommentItem.js
@@ -2,26 +2,32 @@ import React from 'react';
 import { Link } from  'react-router-dom';
 import parse from 'html-react-parser';
 import { FaUserAstronaut } from 'react-icons/fa';
+//  Theme context consumer.
+import ThemeContext from '../contexts/theme';
 
 const CommentItem = ({ by, text, time}) => (
-    <li className='comment'>
-        {/* TODO add avatar*/}
-        <span className='avatar' style={{fontSize: '16px'}}>
-            <FaUserAstronaut color='gold' size={26} />
-        </span>
-        <div className='content'>
-            <Link
-                className='author'
-            >
-                {`by ${by} `}
-            </Link>
-            <div className='metadata'>
-                <span>{`on ${new Date(time *1000).toLocaleString()}`}</span>
-            </div>
-            <div className='text'>{parse(String(text))}</div>
-        </div>
-        <div className='ui divider'></div>
-    </li>
+    <ThemeContext.Consumer>
+        {({ theme }) => (
+            <li className='comment'>
+                <span className='avatar' style={{fontSize: '16px'}}>
+                    <FaUserAstronaut color='gold' size={26} />
+                </span>
+                <div className={`content`}>
+                    <Link
+                        className={`author`}
+                        style={{color: theme === 'dark' ? 'blueviolet' : ''}}
+                    >
+                        {`by ${by} `}
+                    </Link>
+                    <div className='metadata'>
+                        <span className={`text-${theme}`}>{`on ${new Date(time *1000).toLocaleString()}`}</span>
+                    </div>
+                    <div className={`text`} style={{ color: theme === 'dark' ? '#dadada' : ''}}>{parse(String(text))}</div>
+                </div>
+                <div className={`ui divider divider-${theme}`} ></div>
+            </li>
+        )}
+    </ThemeContext.Consumer>
 );
 
 export default CommentItem;
