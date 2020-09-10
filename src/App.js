810a865db9e2be975c93fecd0a786e9818249f40
Marius-De-Allie <spectre4300s@yahoo.com>
2020-09-09 22:48:05 -0400

    Refactor App component from a class based component to a functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Nav from './components/Nav';
 import Loading from './components/Loading';
@@ -10,41 +10,30 @@ const LazyNew = React.lazy(() => import('./components/New'));
 const LazyUser = React.lazy(() => import('./components/User'));
 const LazyComments = React.lazy(() => import('./components/Comments'));
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [theme, setTheme] = useState('light');
 
-    this.state = {
-      theme: 'light',
-      toggleTheme: () => {
-        this.setState(prevState => ({
-          theme: prevState.theme === 'light' ? 'dark' : 'light'
-        }));
-      }
-    }
-  }
+  const toggleTheme = () => setTheme((theme) => theme === 'light' ? 'dark' : 'light');
 
-  render() {
-    return (
-      <ThemeContext.Provider value={this.state}>
-        <div className={this.state.theme}>
-          <div className='ui container'>
-            <Nav />
-            <React.Suspense fallback={Loading}>
-              <Switch>
-                <div className='main-content-container' style={{maxWidth: '1000px', margin: '0 auto'}}>
-                  <Route exact path='/' component={LazyHome} />
-                  <Route path='/new' component={LazyNew} />
-                  <Route path='/user' component={LazyUser} />
-                  <Route path='/post' component={LazyComments} />
-                </div>
-              </Switch>
-            </React.Suspense>
-          </div>
+  return (
+    <ThemeContext.Provider value={theme}>
+      <div className={theme}>
+        <div className='ui container'>
+          <Nav toggleTheme={toggleTheme} />
+          <React.Suspense fallback={Loading}>
+            <Switch>
+              <div className='main-content-container' style={{maxWidth: '1000px', margin: '0 auto'}}>
+                <Route exact path='/' component={LazyHome} />
+                <Route path='/new' component={LazyNew} />
+                <Route path='/user' component={LazyUser} />
+                <Route path='/post' component={LazyComments} />
+              </div>
+            </Switch>
+          </React.Suspense>
         </div>
-      </ThemeContext.Provider>
-    );
-  }
+      </div>
+    </ThemeContext.Provider>
+  );
 };
 
 export default App;
