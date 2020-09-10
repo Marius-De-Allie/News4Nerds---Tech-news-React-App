ae40ead84422e78fafb220408ae9d75cf8fbd2df
Marius-De-Allie <spectre4300s@yahoo.com>
2020-09-09 23:39:40 -0400

    Refactor Home component from a class based component to functional component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,61 +1,94 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import StoryList from './StoryList';
 import { fetchStoryIds, fetchItem } from '../utils/api';
 import ThemeContext from '../contexts/theme';
 
-class Home extends Component {
+const Home = () => {
+    const [topStories, setTopStories] = useState({});
 
-    constructor(props) {
-        super(props);
+    const ids = Object.keys(topStories);
+    const stories = ids.map(id => topStories[id]);
 
-        this.state = {
-            topStories: {}
-        }
-    }
-
-    async componentDidMount () {
-        const storyIds = await fetchStoryIds('topstories');
-        console.log('TOP STORIES', storyIds)
-        storyIds.forEach( async(id) => {
-            if(!this.state.topStories[id]) {
-                const story = await fetchItem(id);
-                this.setState(prevState => ({
-                    topStories: {
-                        ...prevState.topStories,
+    useEffect(() => {
+        (async() => {
+            const storyIds = await fetchStoryIds('topstories');
+            storyIds.forEach(async(id) => {
+                if(!topStories[id]) {
+                    const story = await fetchItem(id);
+                    setTopStories((topStories) => ({
+                        ...topStories,
                         [story.id]: {...story}
-                    }
-                }))
-            }
-        })
+                    }));
+                }
+            });
 
-        // fetchStories('topstories')
-        // .then(topStories => {
-        //   this.setState(prevState => ({
-        //     topStories
-        //   }));
-        // })
-        // .catch(e => console.warn(e));
-    }
+        })();
+    }, [topStories]);
 
-    render() {
-        const { topStories } = this.state;
-        const ids = Object.keys(topStories);
-        const stories = ids.map(id => topStories[id]);
+    return (
+        <ThemeContext.Consumer>
+            {({ theme }) => (
+                <React.Fragment>
+                    <h1 className={`ui header text-${theme}`}>Top 50 Stories</h1>
+                    <StoryList stories={stories} />
+                </React.Fragment>
+            )}
+        </ThemeContext.Consumer>
+    );
+}
+// class Home extends Component {
 
-        // console.log('STORIES', stories)
-        console.log('TOP STORIES MAN', topStories)
-        return (
-            <ThemeContext.Consumer>
-                {({ theme }) => (
-                    <React.Fragment>
-                        <h1 className={`ui header text-${theme}`}>Top 50 Stories</h1>
-                        <StoryList stories={stories} />
-                    </React.Fragment>
-                )}
-            </ThemeContext.Consumer>
-        );
-    }
+//     constructor(props) {
+//         super(props);
 
-};
+//         this.state = {
+//             topStories: {}
+//         }
+//     }
+
+//     async componentDidMount () {
+//         const storyIds = await fetchStoryIds('topstories');
+//         console.log('TOP STORIES', storyIds)
+//         storyIds.forEach( async(id) => {
+//             if(!this.state.topStories[id]) {
+//                 const story = await fetchItem(id);
+//                 this.setState(prevState => ({
+//                     topStories: {
+//                         ...prevState.topStories,
+//                         [story.id]: {...story}
+//                     }
+//                 }))
+//             }
+//         })
+
+//         // fetchStories('topstories')
+//         // .then(topStories => {
+//         //   this.setState(prevState => ({
+//         //     topStories
+//         //   }));
+//         // })
+//         // .catch(e => console.warn(e));
+//     }
+
+//     render() {
+//         const { topStories } = this.state;
+//         const ids = Object.keys(topStories);
+//         const stories = ids.map(id => topStories[id]);
+
+//         // console.log('STORIES', stories)
+//         console.log('TOP STORIES MAN', topStories)
+//         return (
+//             <ThemeContext.Consumer>
+//                 {({ theme }) => (
+//                     <React.Fragment>
+//                         <h1 className={`ui header text-${theme}`}>Top 50 Stories</h1>
+//                         <StoryList stories={stories} />
+//                     </React.Fragment>
+//                 )}
+//             </ThemeContext.Consumer>
+//         );
+//     }
+
+// };
 
 export default Home;
