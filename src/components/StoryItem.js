3a038d610b5a6cfde5860547f48eb6c2c0aaf9b5
Marius-De-Allie <spectre4300s@yahoo.com>
2020-09-11 15:34:57 -0400

    Refactor StoryItem component to use useContext hook in place of ThemeContext.Consumer component and render props

--- src/components/StoryItem.js
+++ src/components/StoryItem.js
@@ -1,48 +1,48 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import ThemeContext from '../contexts/theme';
 
-const StoryItem = ({ by, descendants, id, kids, score, time, title, type, url }) => (
-    <ThemeContext.Consumer>
-        {({ theme }) => (
-            <li className={`ui fluid raised card bg-${theme}`}>
-                <div className='content'>
-                    <a 
-                        className='header story-link'
-                        href={url} target='_blank' 
-                        rel='noopener noreferrer'
+const StoryItem = ({ by, descendants, id, kids, score, time, title, type, url }) => {
+    const theme = useContext(ThemeContext);
+
+    return (
+        <li className={`ui fluid raised card bg-${theme}`}>
+            <div className='content'>
+                <a 
+                    className='header story-link'
+                    href={url} target='_blank' 
+                    rel='noopener noreferrer'
+                >
+                    <h2 className={`text-${theme}`}>{title}</h2>
+                </a>
+            </div>
+            <div className='content'>
+                <p className={`description text-${theme}`}>
+                    <span id='user-avatar'>{'👨‍💻'}</span><span>Posted by</span>
+                    <Link
+                        to={{
+                            pathname: '/user',
+                            search: `?id=${by}`
+                        }}
                     >
-                        <h2 className={`text-${theme}`}>{title}</h2>
-                    </a>
-                </div>
-                <div className='content'>
-                    <p className={`description text-${theme}`}>
-                        <span id='user-avatar'>{'👨‍💻'}</span><span>Posted by</span>
+                        {` ${by} `}
+                    </Link>
+                    on {`${new Date((time * 1000)).toLocaleString()},`}
+                    {descendants > 0 ? 
                         <Link
                             to={{
-                                pathname: '/user',
-                                search: `?id=${by}`
+                                pathname: '/post',
+                                search: `?id=${id}`
                             }}
                         >
-                            {` ${by} `}
-                        </Link>
-                        on {`${new Date((time * 1000)).toLocaleString()},`}
-                        {descendants > 0 ? 
-                            <Link
-                                to={{
-                                    pathname: '/post',
-                                    search: `?id=${id}`
-                                }}
-                            >
-                                {` ${descendants} `}
-                            </Link> : 
-                            ' 0 '}comments
-                    </p>
-                </div>
-            </li>
-        )}
-    </ThemeContext.Consumer>
-);
+                            {` ${descendants} `}
+                        </Link> : 
+                        ' 0 '}comments
+                </p>
+            </div>
+        </li>
+    );
+};
 
 export default StoryItem;
 
