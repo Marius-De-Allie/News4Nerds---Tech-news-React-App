d87de2d5eb05ff3ae701299f2d5a6e5f9fdadf08
Marius-De-Allie <spectre4300s@yahoo.com>
2020-09-11 11:31:14 -0400

    Refactor CommentItem component to use useContext in place of ThemeContext.Consumer component and render props

--- src/components/CommentItem.js
+++ src/components/CommentItem.js
@@ -1,33 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from  'react-router-dom';
 import parse from 'html-react-parser';
 import { FaUserAstronaut } from 'react-icons/fa';
 //  Theme context consumer.
 import ThemeContext from '../contexts/theme';
 
-const CommentItem = ({ by, text, time}) => (
-    <ThemeContext.Consumer>
-        {({ theme }) => (
-            <li className='comment'>
-                <span className='avatar' style={{fontSize: '16px'}}>
-                    <FaUserAstronaut color='gold' size={26} />
-                </span>
-                <div className={`content`}>
-                    <Link
-                        className={`author`}
-                        style={{color: theme === 'dark' ? '#0e7eed' : ''}}
-                    >
-                        {`by ${by} `}
-                    </Link>
-                    <div className='metadata'>
-                        <span className={`text-${theme}`}>{`on ${new Date(time *1000).toLocaleString()}`}</span>
-                    </div>
-                    <div className={`text`} style={{ color: theme === 'dark' ? '#dadada' : ''}}>{parse(String(text))}</div>
+const CommentItem = ({ by, text, time}) => {
+    const theme = useContext(ThemeContext);
+
+    return (
+        <li className='comment'>
+            <span className='avatar' style={{fontSize: '16px'}}>
+                <FaUserAstronaut color='gold' size={26} />
+            </span>
+            <div className={`content`}>
+                <Link
+                    className={`author`}
+                    style={{color: theme === 'dark' ? '#0e7eed' : ''}}
+                >
+                    {`by ${by} `}
+                </Link>
+                <div className='metadata'>
+                    <span className={`text-${theme}`}>{`on ${new Date(time *1000).toLocaleString()}`}</span>
                 </div>
-                <div className={`ui divider divider-${theme}`} ></div>
-            </li>
-        )}
-    </ThemeContext.Consumer>
-);
+                <div className={`text`} style={{ color: theme === 'dark' ? '#dadada' : ''}}>{parse(String(text))}</div>
+            </div>
+            <div className={`ui divider divider-${theme}`} ></div>
+        </li>
+    );
+};
 
 export default CommentItem;
