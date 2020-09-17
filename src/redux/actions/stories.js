820fe729951106aba031cf12082bb83ccecfcd0b
Marius-De-Allie <spectre4300s@yahoo.com>
2020-09-16 23:16:34 -0400

    Create receiveInitialStories and handleReceiveInitialStories action creators

---
+++ src/redux/actions/stories.js
@@ -0,0 +1,21 @@
+import { fetchStoryIds, fetchAllStories } from '../../utils/api';
+
+const RECEIVE_INITIAL_STORIES = 'RECEIVE_INITIAL_STORIES';
+
+const receiveInitialStories = (stories, storyType) => ({
+  type: RECEIVE_INITIAL_STORIES,
+  stories,
+  storyType
+});
+
+// thunk action creator.
+const handleReceiveInitialStories = (type) => {
+  return (dispatch) => {
+    fetchStoryIds(type)
+      .then(ids => {
+        fetchAllStories(ids)
+          .then(stories => dispatch(receiveInitialStories(stories, type)));
+      })
+
+  }
+}
