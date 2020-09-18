7f1af0a327769f9bb87f48f9747a5e1187b822d9
Marius-De-Allie <spectre4300s@yahoo.com>
2020-09-17 21:57:35 -0400

    Add Logo to Nav component and update nav-bar styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
-import { NavLink } from 'react-router-dom';
-import { FaAngleDoubleUp, FaStar } from 'react-icons/fa';
+import { NavLink, useHistory } from 'react-router-dom';
+import { FaAngleDoubleUp, FaStar, FaGlobe } from 'react-icons/fa';
 import ThemeButton from './ThemeButton';
 import TooltipWithHover from './Tooltip';
 import ThemeContext from '../contexts/theme';
@@ -8,32 +8,36 @@ import ThemeContext from '../contexts/theme';
 
 const Nav = ({ toggleTheme }) => {
     const theme = useContext(ThemeContext);
+    const history = useHistory();
 
     return (
         <nav className='nav-bar'>
-            <ul className='nav-items'>
-                <NavLink 
-                    to='/' 
-                    activeClassName='active'
-                    className={`nav-links text-${theme}`}
-                    exact
-                >
-                <FaAngleDoubleUp />
-                    {` Top`}
-                </NavLink>
-                <NavLink 
-                    to='/new' 
-                    activeClassName='active '
-                    className={`nav-links text-${theme}`}                   
-                    exact
-                >
-                <FaStar />
-                    {` New`}
-                </NavLink>
-            </ul>
-            <TooltipWithHover tooltip='Switch theme dark\light'>
-                <ThemeButton toggleTheme={toggleTheme} />
-            </TooltipWithHover>
+            <FaGlobe id='logo' size={65} onClick={() => history.push('/')} />
+            <div className='nav-container'>
+                <ul className='nav-items'>
+                    <NavLink 
+                        to='/' 
+                        activeClassName='active'
+                        className={`nav-links text-${theme}`}
+                        exact
+                    >
+                    <FaAngleDoubleUp />
+                        {` Top`}
+                    </NavLink>
+                    <NavLink 
+                        to='/new' 
+                        activeClassName='active '
+                        className={`nav-links text-${theme}`}                   
+                        exact
+                    >
+                    <FaStar />
+                        {` New`}
+                    </NavLink>
+                </ul>
+                <TooltipWithHover tooltip='Switch theme dark\light'>
+                    <ThemeButton className='theme-btn' toggleTheme={toggleTheme} />
+                </TooltipWithHover>
+            </div>
         </nav>
     );
 };
